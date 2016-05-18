0102d491932af553c2feecc01c8575a2ef5ef485
lucasey <lucasey@126.com>
2016-05-18 14:12:02 +0800

    初始导入

---
+++ PhotoAibum/src/com/why/photoaibum/ShowBigPic.java
@@ -0,0 +1,30 @@
+package com.why.photoaibum;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.widget.ImageView;
+
+public class ShowBigPic extends Activity{
+	ImageView  iv_show_big_pic;
+	String path;
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.show_big_pic);
+		iv_show_big_pic=(ImageView)findViewById(R.id.iv_show_big_pic);
+		path=getIntent().getStringExtra("path");
+		iv_show_big_pic.setImageBitmap(BitmapFactory.decodeFile(path));
+	}
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+    	switch (event.getAction()) {
+		case MotionEvent.ACTION_DOWN:
+			finish();
+			break;
+		}
+    	return super.onTouchEvent(event);
+    }
+}
